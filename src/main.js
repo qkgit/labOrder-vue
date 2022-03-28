2620d60111f7f02df96e8019e69b90979aa187f0
qkgit <1584546760@qq.com>
2022-03-28 17:29:15 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 import Vue from 'vue'
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets | css重置的现代替代方案
+import 'normalize.css/normalize.css' //  css重置的现代替代方案
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import locale from 'element-ui/lib/locale/lang/en' // lang i18n | 多语言
+// import locale from 'element-ui/lib/locale/lang/en' // 多语言
 
-import '@/styles/index.scss' // global css | 通用css
+import '@/styles/index.scss' // 通用css
 import '@/styles/index.css' // 自定义样式
 
 import App from './App'
 import store from './store'
 import router from './router'
 
-import '@/icons' // icon | 图标
-import '@/permission' // permission control | 权限控制
+import '@/icons' //  图标
+import '@/permission' // 权限控制
+
 import { getDicts } from '@/api/system/dict.js' // 获取字典项
-import { parseTime, resetForm, addDateRange, selectDictLabel, selectDictLabels, download, handleTree } from '@/utils/baseUtils'
+import { parseTime, resetForm, addDateRange, selectDictLabel, selectDictLabels, download, handleTree,digital2Chinese } from '@/utils/baseUtils'
 import qs from 'qs'     
-Vue.prototype.$qs = qs
+
 // import { from } from 'core-js/core/array'
 /**
  * 如果您不想使用模拟服务器 mock-server
- * you want to use MockJs for mock api | 你想为mock api使用MockJs
- * you can execute: mockXHR() | 你可以执行以下命令: mockXHR()
+ * 你想为mock api使用MockJs
+ * 你可以执行命令: mockXHR()
  *
- * Currently MockJs will be used in the production environment, | 目前，MockJs将用于生产环境，
- * please remove it before going online ! ! ! | 请在上线前删除它！！！
+ * 目前，MockJs将用于生产环境，
+ * 请在上线前删除它！！！
  */
 // if (process.env.NODE_ENV === 'production') {
 //   const { mockXHR } = require('../mock')
@@ -36,8 +37,9 @@ Vue.prototype.$qs = qs
 // console.log(process.env.VUE_APP_BASE_API)
 
 // 全局方法挂载
+Vue.prototype.$qs = qs
 Vue.prototype.getDicts = getDicts
-// Vue.prototype.getConfigKey = getConfigKey
+Vue.prototype.digital2Chinese = digital2Chinese
 Vue.prototype.parseTime = parseTime
 Vue.prototype.resetForm = resetForm
 Vue.prototype.addDateRange = addDateRange
