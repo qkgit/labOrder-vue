0cdb7cd1e477d5d42f44701fd45f951186ca19eb
qkgit <1584546760@qq.com>
2022-04-07 17:50:34 +0800

    教室预约

--- src/views/order/class/index.vue
+++ src/views/order/class/index.vue
@@ -2,290 +2,197 @@
   <div class="container">
     <div class="rule-top">
       <el-row class="item">
-        <el-radio-group v-model="item.week" @tab-click="fetchData">
-          <el-radio-button label="Monday">周一</el-radio-button>
-          <el-radio-button label="Tuesday">周二</el-radio-button>
-          <el-radio-button label="Wednesday">周三</el-radio-button>
-          <el-radio-button label="Thursday">周四</el-radio-button>
-          <el-radio-button label="Friday">周五</el-radio-button>
-          <!-- <el-radio-button label="" disabled >    </el-radio-button> -->
-          <el-radio-button label="Saturday">周六</el-radio-button>
-          <el-radio-button label="Sunday">周日</el-radio-button>
+        <el-radio-group v-model="queryParam.week" @change="fetchWeek">
+          <el-radio-button label="1">周一</el-radio-button>
+          <el-radio-button label="2">周二</el-radio-button>
+          <el-radio-button label="3">周三</el-radio-button>
+          <el-radio-button label="4">周四</el-radio-button>
+          <el-radio-button label="5">周五</el-radio-button>
+          <el-radio-button label="6">周六</el-radio-button>
+          <el-radio-button label="7">周日</el-radio-button>
         </el-radio-group>
       </el-row>
       <el-row>
-        <el-tabs
-          v-model="item.classHour"
-          style="width: 730px"
-          @tab-click="fetchData"
-        >
-          <el-tab-pane label="第一节课" name="first" />
-          <el-tab-pane label="第二节课" name="second" />
-          <el-tab-pane label="第三节课" name="third" />
-          <el-tab-pane label="第四节课" name="fourth" />
-          <el-tab-pane label="第五节课" name="fifth" />
-          <el-tab-pane label="第六节课" name="sixth" />
-          <el-tab-pane label="第七节课" name="seventh" />
-          <el-tab-pane label="第八节课" name="eighth" />
+        <el-tabs v-model="queryParam.courseNode" @tab-click="fetchNode">
+          <el-tab-pane
+            v-for="(t, i) in timeList"
+            :key="i + 1"
+            :label="'第' + digital2Chinese(i + 1) + '节课'"
+            :name="(i + 1).toString()"
+          />
         </el-tabs>
       </el-row>
     </div>
     <div style="">
       <el-card shadow="never" style="width: 10%; float: right">
         <el-tabs
-          v-model="item.floor"
+          v-model="queryParam.floor"
           tab-position="left"
           style="height: 180px"
-          @tab-click="fetchData"
+          @tab-click="fetchFloor"
         >
-          <el-tab-pane label="四层" name="four" />
-          <el-tab-pane label="三层" name="three" />
-          <el-tab-pane label="二层" name="two" />
-          <el-tab-pane label="一层" name="one" />
+          <el-tab-pane label="四层" name="4" />
+          <el-tab-pane label="三层" name="3" />
+          <el-tab-pane label="二层" name="2" />
+          <el-tab-pane label="一层" name="1" />
         </el-tabs>
       </el-card>
 
-      <div class="room_background">
-        <div class="room_1">
-          <el-card class="box-card" shadow="hover">
-            教室1
-          </el-card>
+      <div
+        :class="queryParam.floor > 1 ? 'room_background_2' : 'room_background'"
+      >
+        <div class="row_1">
+          <div class="room_1">
+            <el-card class="box-card" shadow="hover" @click.native="handleOrder"> 教室1 </el-card>
+          </div>
+          <div class="room_2">
+            <el-card class="box-card" shadow="hover" @click.native="handleOrder"> 教室2 </el-card>
+          </div>
+          <div class="room_3">
+            <el-card class="box-card" shadow="hover" @click.native="handleOrder"> 教室3 </el-card>
+          </div>
         </div>
-        <div class="room_2">
-          <el-card class="box-card" shadow="hover">
-            教室2
-          </el-card>
+        <div class="row_2">
+          <div class="room_4">
+            <el-card class="box-card" shadow="hover" @click.native="handleOrder"> 教室4 </el-card>
+          </div>
+          <div class="room_5">
+            <el-card class="box-card" shadow="hover" @click.native="handleOrder"> 教室5 </el-card>
+          </div>
+          <div class="room_6">
+            <el-card class="box-card" shadow="hover" @click.native="handleOrder"> 教室6 </el-card>
+          </div>
         </div>
-        <div class="room_3">
-          <el-card class="box-card" shadow="hover">
-            教室3
-          </el-card>
+        <div class="row_3">
+          <div class="room_7">
+            <el-card class="box-card-7" shadow="hover" @click.native="handleOrder"> 教室7 </el-card>
+          </div>
+          <div class="room_8">
+            <el-card class="box-card-8" shadow="hover" @click.native="handleOrder"> 教室8 </el-card>
+          </div>
         </div>
-        <div class="room_4">
-          <el-card class="box-card" shadow="hover">
-            教室4
-          </el-card>
+        <div class="room_9">
+          <el-card class="box-card-9" shadow="hover" @click.native="handleOrder"> 教室9 </el-card>
         </div>
-        <div class="room_5">
-          <el-card class="box-card" shadow="hover">
-            教室5
-          </el-card>
+        <div class="col_1">
+          <div class="room_10">
+            <el-card class="box-card-10" shadow="hover" @click.native="handleOrder"> 教室10 </el-card>
+          </div>
         </div>
-        <div class="room_6">
-          <el-card class="box-card" shadow="hover">
-            教室6
-          </el-card>
-        </div>
-        <!-- <div
-          v-for="i in 20"
-          :key="i"
-          style="display: inline-block; margin: 5px"
-        >
-          <el-card class="box-card" shadow="hover" @click.native="handleClick">
-            <p class="classStyle">{{ "教室 " + i }}</p>
-            <p class="classStyle">{{ "预约中..." }}</p>
-            <p class="classStyle">{{ "当前人数：xx" }}</p>
-          </el-card>
-        </div> -->
       </div>
     </div>
-    <coursesTable />
-    <!-- <timetables></timetables> -->
   </div>
 </template>
 
 <script>
-import Timetables from "timetables";
+import courseApi from "@/api/system/course";
 
 export default {
   data() {
     return {
-      item: {
-        classHour: "first", // 课程数
-        week: "", // 星期
-        floor: "one", // 楼层
-        dateTime: "", // 当前时间
+      // 查询参数
+      queryParam: {
+        // 课程节数
+        courseNode: "",
+        // 星期
+        week: "",
+        // 楼层
+        floor: "1",
       },
+      timeList: [],
     };
   },
-
   created() {
-    const timetables = [
-      [
-        "大学英语(Ⅳ)@10203",
-        "大学英语(Ⅳ)@10203",
-        "",
-        "",
-        "",
-        "",
-        "毛概@14208",
-        "毛概@14208",
-        "",
-        "",
-        "",
-        "选修",
-      ],
-      [
-        "",
-        "",
-        "信号与系统@11302",
-        "信号与系统@11302",
-        "模拟电子技术基础@16204",
-        "模拟电子技术基础@16204",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-      ],
-      [
-        "大学体育(Ⅳ)",
-        "大学体育(Ⅳ)",
-        "形势与政策(Ⅳ)@15208",
-        "形势与政策(Ⅳ)@15208",
-        "",
-        "",
-        "电路、信号与系统实验",
-        "电路、信号与系统实验",
-        "",
-        "",
-        "",
-        "",
-      ],
-      [
-        "",
-        "",
-        "",
-        "",
-        "电装实习@11301",
-        "电装实习@11301",
-        "",
-        "",
-        "",
-        "大学体育",
-        "大学体育",
-        "",
-      ],
-      [
-        "",
-        "",
-        "数据结构与算法分析",
-        "数据结构与算法分析",
-        "",
-        "",
-        "",
-        "",
-        "信号与系统",
-        "信号与系统",
-        "",
-        "",
-      ],
-    ];
-    const timetableType = [
-      [{ index: "1", name: "8:30" }, 1],
-      [{ index: "2", name: "9:30" }, 1],
-      [{ index: "3", name: "10:30" }, 1],
-      [{ index: "4", name: "11:30" }, 1],
-      [{ index: "5", name: "12:30" }, 1],
-      [{ index: "6", name: "14:30" }, 1],
-      [{ index: "7", name: "15:30" }, 1],
-      [{ index: "8", name: "16:30" }, 1],
-      [{ index: "9", name: "17:30" }, 1],
-      [{ index: "10", name: "18:30" }, 1],
-      [{ index: "11", name: "19:30" }, 1],
-      [{ index: "12", name: "20:30" }, 1],
-    ];
-    const week = ["周一", "周二", "周三", "周四", "周五"];
-    const highlightWeek = new Date().getDay();
-    const styles = {
-      Gheight: 50,
-      leftHandWidth: 50,
-      palette: ["#ff6633", "#eeeeee"],
-    };
-
-    // 实例化(初始化课表)
-    const timetable = new Timetables({
-      el: "#coursesTable",
-      timetables: timetables,
-      week: week,
-      timetableType: timetableType,
-      highlightWeek: highlightWeek,
-      gridOnClick: function (e) {
-        alert(
-          e.name +
-            "  " +
-            e.week +
-            ", 第" +
-            e.index +
-            "节课, 课长" +
-            e.length +
-            "节"
-        );
-        console.log(e);
-      },
-      styles: styles,
-    });
-
-    this.getTime();
+    this.initParamAndQuery();
   },
   methods: {
-    fetchData(tab, event) {
-      console.log(tab, event);
+    /**
+     *  初始化数据并查询
+     */
+    initParamAndQuery() {
+      courseApi
+        .getDefaultTime()
+        .then((res) => {
+          const defaultTime = res.data;
+          this.timeList = defaultTime.times;
+        })
+        .then(() => {
+          // 星期
+          const wk = new Date().getDay();
+          this.queryParam.week = wk;
+          // 楼层
+          this.queryParam.floor = "1";
+          // 判断当前时间所处节数
+          var now = new Date();
+          var node = 1;
+          var maxNode = this.timeList.length + 1;
+          this.timeList.forEach((t, i) => {
+            var start = t.startTime.split(":");
+            var end = t.endTime.split(":");
+            var startTime = new Date();
+            var endTime = new Date();
+            startTime.setHours(start[0]);
+            startTime.setMinutes(start[1]);
+            startTime.setSeconds(start[2]);
+            endTime.setHours(end[0]);
+            endTime.setMinutes(end[1]);
+            endTime.setSeconds(end[2]);
+            if (now >= startTime && now <= endTime) {
+              this.queryParam.courseNode =
+                node > maxNode ? "1" : node.toString();
+            } else {
+              node = node + 1;
+            }
+          });
+        })
+        .then(() => {
+          this.getList();
+        });
     },
+
+    getList() {
+      console.log("=> getList");
+      console.log(
+        "=>周" +
+          this.queryParam.week +
+          " 第" +
+          this.queryParam.courseNode +
+          "节 " +
+          this.queryParam.floor +
+          "层"
+      );
+    },
+
     // 预约
-    handleOrder(id) {
-      this.$confirm("确认预约该实验室吗？", "提示", {
+    handleOrder(row) {
+      this.$confirm("确认预约该教室吗?", "提示", {
         cancelButtonText: "取消",
         confirmButtonText: "确认",
         type: "warning",
       })
-        .then(() => {
-          // 确认
-          var that = this;
-          orderApi.orderLab(id).then((response) => {
-            // 提示信息
-            this.$message({
-              type: response.resultCode == 200 ? "success" : "error",
-              message: response.message,
-            });
-            if (response.resultCode == 200) {
-              // 删除成功 刷新列表
-              that.fetchData(this.pageQuery);
-            }
-          });
+        .then(function(){
+          return orderApi.orderLab(row.uuid)
         })
-        .catch(() => {
-          // 取消删除 不理会
-        });
+        .then((response) => {
+          this.getList();
+        })
+        .catch(() => {});
     },
-    // 获取当前时间
-    getTime() {
-      // 课时
-      var _this = this;
-      const hh = new Date().getHours();
-      const mf =
-        new Date().getMinutes() < 10
-          ? "0" + new Date().getMinutes()
-          : new Date().getMinutes();
-      const ss =
-        new Date().getSeconds() < 10
-          ? "0" + new Date().getSeconds()
-          : new Date().getSeconds();
-      _this.item.dateTime = hh + ":" + mf + ":" + ss;
-      // 星期
-      const wk = new Date().getDay();
-      const weeks = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-      const week = weeks[wk];
-      _this.item.week = week;
+
+    // 选择周
+    fetchWeek(value) {
+      this.queryParam.week = value;
+      this.getList();
+    },
+    // 选择节数
+    fetchNode(tab, event) {
+      this.queryParam.courseNode = tab.name;
+      this.getList();
+    },
+    // 选择楼层
+    fetchFloor(tab, event) {
+      this.queryParam.floor = tab.name;
+      this.getList();
     },
   },
 };
@@ -294,80 +201,127 @@ export default {
 <style>
 .rule-top {
 }
-.rule-top item {
+.rule-top .item {
   margin: 0 15px;
-}
-.text {
-  font-size: 14px;
-}
-
-.item {
   padding: 18px 0;
 }
-
 .box-card {
-  width: 121px;
-  height: 65px;
+  width: 118px;
+  height: 60px;
   background: #f3f3f3;
   cursor: pointer;
 }
-.query-box {
-  width: 80%;
-  float: left;
+.box-card-7 {
+  width: 108px;
+  height: 71px;
+  background: #f3f3f3;
+  cursor: pointer;
 }
-.classStyle {
-  margin: 0;
+.box-card-8 {
+  width: 107px;
+  height: 71px;
+  background: #f3f3f3;
+  cursor: pointer;
+}
+.box-card-9 {
+  width: 120px;
+  height: 88px;
+  background: #f3f3f3;
+  cursor: pointer;
+}
+.box-card-10 {
+  width: 243px;
+  height: 241px;
+  background: #f3f3f3;
+  cursor: pointer;
 }
 .room_background {
   background: url("../../../assets/floor_pic/1.jpg");
-  margin-top: -100px;
-  height: 800px;
-  width: 85%;
+  background-position: 0px -80px;
+  background-repeat: no-repeat;
+  height: 640px;
+  width: 1060px;
   float: left;
 }
-.room_background .room_1 {
-  height: 65px;
-  width: 121px;
-  margin-top: 148px;
-  margin-left: 317px;
+.room_background_2 {
+  background: url("../../../assets/floor_pic/2.jpg");
+  background-position: 0px -80px;
+  background-repeat: no-repeat;
+  height: 640px;
+  width: 1060px;
+  float: left;
+}
+.row_1 {
+  margin-top: 70px;
+  width: 65%;
+  float: left;
+}
+.row_2 {
+  margin-top: 55px;
+  width: 65%;
+  float: left;
+}
+.row_3 {
+  margin-top: 278px;
+  width: 53%;
+  float: left;
+}
+.col_1 {
   display: inline-block;
-
+  margin-top: 84px;
+  margin-left: 85px;
 }
-.room_background .room_2 {
-  height: 65px;
-  width: 121px;
-  margin-top: 148px;
+.room_1 {
+  height: 60px;
+  width: 123px;
+  margin-left: 319px;
   display: inline-block;
-      margin-left: 5px;
-
 }
-.room_background .room_3 {
-  height: 65px;
-  width: 121px;
-  margin-top: 148px;
+.room_2 {
+  height: 60px;
+  width: 123px;
   display: inline-block;
-      margin-left: 5px;
-
 }
-.room_background .room_4 {
-  height: 65px;
-  width: 121px;
-  margin-left: 317px;
- 
-
-}
-.room_background .room_5 {
-  height: 65px;
-  width: 121px;
-  margin-left: 350px;
- 
-
-}
-.room_background .room_6 {
-  height: 65px;
-  width: 121px;
-  margin-left: 450px;
-
+.room_3 {
+  height: 60px;
+  width: 123px;
+  display: inline-block;
 }
 
+.room_4 {
+  height: 60px;
+  width: 123px;
+  margin-left: 319px;
+  display: inline-block;
+}
+.room_5 {
+  height: 60px;
+  width: 123px;
+  display: inline-block;
+}
+.room_6 {
+  height: 60px;
+  width: 123px;
+  display: inline-block;
+}
+
+.room_7 {
+  height: 71px;
+  width: 115px;
+  margin-left: 339px;
+  display: inline-block;
+}
+.room_8 {
+  height: 71px;
+  width: 107px;
+  display: inline-block;
+}
+.room_9 {
+  height: 88px;
+  width: 120px;
+  display: inline-block;
+  margin-top: 278px;
+  margin-left: 5px;
+  float: left;
+}
 </style>
